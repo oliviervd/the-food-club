2980559de2e768bcf5593666b478f60f744c13c7
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-12 10:57:28 +0200

    add collection: venues.

---
+++ src/collections/Venues.ts
@@ -0,0 +1,31 @@
+import {CollectionConfig} from "payload/types";
+import address from "../fields/address";
+import {hasAccessOrPublished} from "../access/hasAccessOrPublished";
+import {isAdmin} from "../access/isAdmin";
+import {isEditor} from "../access/isEditor";
+import contentStatus from "../fields/contentStatus";
+
+const Venues:CollectionConfig = {
+    slug: "venue",
+    admin: {
+        useAsTitle: "venueName"
+    },
+    access: {
+        read: hasAccessOrPublished,
+        create: isEditor,
+        update: isEditor,
+        delete: isAdmin
+    },
+    fields: [
+        {
+            name: "venueName",
+            label: "venue name",
+            type: "text",
+            required: true
+        },
+        address,
+        contentStatus
+    ]
+}
+
+export default Venues
