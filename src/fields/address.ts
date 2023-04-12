57e92e67c362fa47d563382ab65d15a4c206c044
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-12 10:57:20 +0200

    add field for address management

---
+++ src/fields/address.ts
@@ -0,0 +1,35 @@
+import {Field} from "payload/types";
+
+const address: Field = {
+    type: "row",
+    fields: [
+        {
+            name: "street",
+            label: "street",
+            type: "text",
+            required: true
+        },
+        {
+            name: "houseNumber",
+            label: "house number",
+            type: "text",
+            required: true
+        },
+        {
+            name: "city",
+            label: "city",
+            type: "text",
+            required: true,
+            defaultValue: ["Gent"]
+        },
+        {
+            name: "postalCode",
+            label: "postal code",
+            type: "text",
+            required: true,
+            defaultValue: ["9000"]
+        }
+    ]
+}
+
+export default address
