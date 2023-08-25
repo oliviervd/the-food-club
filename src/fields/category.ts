6d69d60d56fcb12484557dece71962a12f17b993
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-08-25 11:41:40 +0200

    update category

--- src/fields/category.ts
+++ src/fields/category.ts
@@ -1,10 +1,15 @@
 import {Field} from "payload/types";
+import {isEditor} from "../access/isEditor";
+import {isAdmin} from "../access/isAdmin";
 
 const Category:Field = {
     name: "category",
     label: "categorie(s)",
     admin: {
         description: "pick the categories to which this venue belongs",
+    },
+    access: {
+        read: () => true
     },
     type: "relationship",
     hasMany: true,
--- src/fields/cuisine.ts
+++ src/fields/cuisine.ts
@@ -4,7 +4,7 @@ const cuisine:Field = {
     name: "cuisineUsed",
     label: "cuisine(s)",
     admin: {
-        description: "pick the appropriate cuisine or cuisines served at this restaurant/venue"
+        description: "pick the appropriate cuisine or cuisines served at this restaurant/venue",
     },
     type: "relationship",
     hasMany: true,
