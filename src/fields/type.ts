5059d5f2863bf76faca2656a53f9db1bf2eaa043
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-25 05:34:15 +0200

    add field for occasion (breakfast, brunch, lunch, dinner, ..)

--- src/fields/type.ts
+++ src/fields/type.ts
@@ -3,6 +3,7 @@ import {Field} from "payload/types";
 const occasion: Field = {
     name: "type",
     type: "select",
+    //todo: make these for different languages
     options: [
         {
             value: "breakfast",
