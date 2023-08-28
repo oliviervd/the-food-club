c0e02b0beed8007855d034539f670079fd94e5da
oliviervd <olliviervandhuynslager@gmail.com>
2023-08-28 18:16:56 +0200

    unpublish notes

--- src/fields/notes.ts
+++ src/fields/notes.ts
@@ -1,6 +1,11 @@
 import {Field} from "payload/types";
+import {isAdminFieldLevel} from "../access/isAdmin";
+import {isEditor} from "../access/isEditor";
 
 const notes:Field = {
+    access: {
+        read: ()=>false // don't publish this field on API.
+    },
     name: 'notes',
         label: "notes",
     type: "richText",
