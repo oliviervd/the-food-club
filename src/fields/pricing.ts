94bb135758446827891b5a87ab7a28e462617a48
oliviervd <olliviervandhuynslager@gmail.com>
2023-09-16 12:58:51 +0200

    update price ranges

--- src/fields/pricing.ts
+++ src/fields/pricing.ts
@@ -3,18 +3,25 @@ import {Field} from "payload/types";
 const pricing: Field = {
     name: "pricing",
     type: "select",
+    admin: {
+        description: "indicative price per head"
+    },
     options: [
         {
-            value: "cheap",
+            value: "€",
             label: "10-20EUR"
         },
         {
-            value: "midrange",
-            label: "20-40EUR"
+            value: "€€",
+            label: "20-30EUR"
         },
         {
-            value: "expensive",
-            label: "40EUR+"
+            value: "€€€",
+            label: "30-50EUR"
+        },
+        {
+            value: "€€€€",
+            label: "50+"
         }
     ]
 }
