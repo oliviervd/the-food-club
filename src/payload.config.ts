ad4a0a78103869af81d7119ea6a2c54c12953d31
oliviervd <oliviervandhuynslager@gmail.com>
2023-12-09 11:43:44 +0100

    connect to cloudFront

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,8 +1,8 @@
-import {buildConfig} from 'payload/config';
-import {cloudStorage} from "@payloadcms/plugin-cloud-storage";
-import {s3Adapter} from "@payloadcms/plugin-cloud-storage/s3";
-import path from 'path';
-import Users from './collections/Users';
+import { buildConfig } from "payload/config";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
+import path from "path";
+import Users from "./collections/Users";
 import Categories from "./collections/Categories";
 import Venues from "./collections/Venues";
 import Cuisine from "./collections/Cuisine";
@@ -10,56 +10,55 @@ import Media from "./collections/Media";
 import Designer from "./collections/Designer";
 
 export default buildConfig({
-    //
-    plugins: [
-        cloudStorage({
-            collections: {
-                'media': {
-                    adapter: s3Adapter({
-                        config: {
-                            endpoint: process.env.S3_ENDPOINT,
-                            region: process.env.S3_REGION,
-                            credentials: {
-                                accessKeyId: process.env.S3_ACCESS_KEY_ID,
-                                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-                            },
-                        },
-                        bucket: process.env.S3_BUCKET,
-                    }),
-                }
+  //
+  plugins: [
+    cloudStorage({
+      collections: {
+        media: {
+          adapter: s3Adapter({
+            config: {
+              endpoint: process.env.S3_ENDPOINT,
+              region: process.env.S3_REGION,
+              credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+              },
             },
-        }),
-    ],
-    serverURL: process.env.PAYLOAD_URL,
-    //CORS
-    cors: [
-        'https://the-food-club-front.vercel.app',
-        'http://localhost:3000',
-        'https://p01--cms--j4bvc8vdjtjb.code.run/',
-        'vitals.vercel-insights.com'
-    ],
-    //CSRF
-    csrf: [
-        'https://the-food-club-front.vercel.app',
-        'http://localhost:3000',
-        'https://p01--cms--j4bvc8vdjtjb.code.run/',
-        'vitals.vercel-insights.com'
-    ],
-    admin: {
-        user: Users.slug,
-    },
-    collections: [
-        Users,
-        Venues,
-        Categories,
-        Cuisine,
-        Designer,
-        Media
-    ],
-    typescript: {
-        outputFile: path.resolve(__dirname, 'payload-types.ts'),
-    },
-    graphQL: {
-        schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
-    },
+            bucket: process.env.S3_BUCKET,
+          }),
+
+          generateFileURL: ({ filename, prefix }) => {
+            return ["https://d3nidktcupd88v.cloudfront.net", prefix, filename]
+              .filter(Boolean)
+              .join("/");
+          },
+        },
+      },
+    }),
+  ],
+  serverURL: process.env.PAYLOAD_URL,
+  //CORS
+  cors: [
+    "https://the-food-club-front.vercel.app",
+    "http://localhost:3000",
+    "https://p01--cms--j4bvc8vdjtjb.code.run/",
+    "vitals.vercel-insights.com",
+  ],
+  //CSRF
+  csrf: [
+    "https://the-food-club-front.vercel.app",
+    "http://localhost:3000",
+    "https://p01--cms--j4bvc8vdjtjb.code.run/",
+    "vitals.vercel-insights.com",
+  ],
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users, Venues, Categories, Cuisine, Designer, Media],
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+  },
 });
