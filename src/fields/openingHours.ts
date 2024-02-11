7fd382e55f726234ab9f9a7aeccaa98674611ca0
oliviervd <oliviervandhuynslager@gmail.com>
2024-02-11 15:40:05 +0100

    implement new strategy for openingshours.

--- src/fields/openingHours.ts
+++ src/fields/openingHours.ts
@@ -10,96 +10,102 @@ const openingHours: Field = {
     name: "hours",
     fields:[
         {
-            name: "openDay",
-            label: {
-                en: "day",
-                fr: "jour",
-                nl: "dag"
-            },
-            type: "select",
-            options: [
+            type: "row",
+            fields: [
                 {
-                    value: "Mo",
+                    name: "openDay",
                     label: {
-                        en: "Monday",
-                        fr: "Lundi",
-                        nl: "maandag"
+                        en: "day",
+                        fr: "jour",
+                        nl: "dag"
+                    },
+                    type: "select",
+                    options: [
+                        {
+                            value: "Mo",
+                            label: {
+                                en: "Monday",
+                                fr: "Lundi",
+                                nl: "maandag"
+                            }
+                        },
+                        {
+                            value: "Tu",
+                            label: {
+                                en: "Tuesday",
+                                fr: "Mardi",
+                                nl: "dinsdag"
+                            }
+                        },
+                        {
+                            value: "We",
+                            label: {
+                                en: "Wednesday",
+                                fr: "Mercredi",
+                                nl: "woensdag"
+                            }
+                        },
+                        {
+                            value: "Thu",
+                            label: {
+                                en: "Thursday",
+                                fr: "Jeudi",
+                                nl: "donderdag"
+                            }
+                        },
+                        {
+                            value: "Fr",
+                            label: {
+                                en: "Friday",
+                                fr: "Vendredi",
+                                nl: "vrijdag"
+                            }
+                        },
+                        {
+                            value: "Sat",
+                            label: {
+                                en: "Saturday",
+                                fr: "Samedi",
+                                nl: "zaterdag"
+                            }
+                        },
+                        {
+                            value: "Sun",
+                            label: {
+                                en: "Sunday",
+                                fr: "Dimanche",
+                                nl: "zondag"
+                            }
+                        },
+                        {
+                            value: "UNDEFINED",
+                            label: "UNDEFINED"
+                        }
+                    ]
+                },
+                {
+                    name: "openFrom",
+                    label: {},
+                    type: "date",
+                    admin: {
+                        date: {
+                            pickerAppearance: "timeOnly"
+                        }
                     }
                 },
                 {
-                    value: "Tu",
-                    label: {
-                        en: "Tuesday",
-                        fr: "Mardi",
-                        nl: "dinsdag"
+                    name: "openTill",
+                    label: {},
+                    type: "date",
+                    admin: {
+                        date: {
+                            pickerAppearance: "timeOnly"
+                        }
                     }
-                },
-                {
-                    value: "We",
-                    label: {
-                        en: "Wednesday",
-                        fr: "Mercredi",
-                        nl: "woensdag"
-                    }
-                },
-                {
-                    value: "Thu",
-                    label: {
-                        en: "Thursday",
-                        fr: "Jeudi",
-                        nl: "donderdag"
-                    }
-                },
-                {
-                    value: "Fr",
-                    label: {
-                        en: "Friday",
-                        fr: "Vendredi",
-                        nl: "vrijdag"
-                    }
-                },
-                {
-                    value: "Sat",
-                    label: {
-                        en: "Saturday",
-                        fr: "Samedi",
-                        nl: "zaterdag"
-                    }
-                },
-                {
-                    value: "Sun",
-                    label: {
-                        en: "Sunday",
-                        fr: "Dimanche",
-                        nl: "zondag"
-                    }
-                },
-                {
-                    value: "UNDEFINED",
-                    label: "UNDEFINED"
                 }
             ]
-        },
-        {
-          name: "openFrom",
-            label: {},
-            type: "date",
-            admin: {
-              date: {
-                  pickerAppearance: "timeOnly"
-              }
-            }
-        },
-        {
-            name: "openTill",
-            label: {},
-            type: "date",
-            admin: {
-                date: {
-                    pickerAppearance: "timeOnly"
-                }
-            }
         }
+
     ]
 }
 
