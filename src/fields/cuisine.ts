39615ebfec1a871c433bf45a1646ee6bf56572c8
oliviervd <oliviervandhuynslager@gmail.com>
2024-02-11 12:40:56 +0100

    add labels localization for field cuisine.ts

--- src/fields/cuisine.ts
+++ src/fields/cuisine.ts
@@ -2,9 +2,17 @@ import {Field} from "payload/types";
 
 const cuisine:Field = {
     name: "cuisineUsed",
-    label: "cuisine(s)",
+    label: {
+        en: "cuisine(s)",
+        fr: "cuisine(s)",
+        nl: "keuken(s)"
+    },
     admin: {
-        description: "pick the appropriate cuisine or cuisines served at this restaurant/venue",
+        description: {
+            en:"pick the appropriate cuisine or cuisines served at this restaurant/venue",
+            fr:"choisir la ou les cuisine(s) appropriée(s) servie(s) dans ce restaurant/lieu",
+            nl:"selecteer de juiste keuken of keukens die in dit restaurant/op deze locatie worden geserveerd"
+        },
     },
     type: "relationship",
     hasMany: true,
