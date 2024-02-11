508b120826e59472a8ac15e303d7ef91c1fda965
oliviervd <oliviervandhuynslager@gmail.com>
2024-02-11 15:01:26 +0100

    implement new strategy for openingshours.

--- src/fields/openingHours.ts
+++ src/fields/openingHours.ts
@@ -1,72 +1,104 @@
 import {Field} from "payload/types";
 
 const openingHours: Field = {
+    type: "array",
+    label: {
+        en: "hours",
+        fr: "horaires",
+        nl: "openingsuren"
+    },
     name: "openOn",
-    type: "select",
-    hasMany: true,
-    required: true,
-    defaultValue: "UNDEFINED",
-    localized: false,
-    options: [
+    fields:[
         {
-            value: "Mo",
+            name: "openDay",
             label: {
-                en: "Monday",
-                fr: "Lundi",
-                nl: "maandag"
+                en: "day",
+                fr: "jour",
+                nl: "dag"
+            },
+            type: "select",
+            options: [
+                {
+                    value: "Mo",
+                    label: {
+                        en: "Monday",
+                        fr: "Lundi",
+                        nl: "maandag"
+                    }
+                },
+                {
+                    value: "Tu",
+                    label: {
+                        en: "Tuesday",
+                        fr: "Mardi",
+                        nl: "dinsdag"
+                    }
+                },
+                {
+                    value: "We",
+                    label: {
+                        en: "Wednesday",
+                        fr: "Mercredi",
+                        nl: "woensdag"
+                    }
+                },
+                {
+                    value: "Thu",
+                    label: {
+                        en: "Thursday",
+                        fr: "Jeudi",
+                        nl: "donderdag"
+                    }
+                },
+                {
+                    value: "Fr",
+                    label: {
+                        en: "Friday",
+                        fr: "Vendredi",
+                        nl: "vrijdag"
+                    }
+                },
+                {
+                    value: "Sat",
+                    label: {
+                        en: "Saturday",
+                        fr: "Samedi",
+                        nl: "zaterdag"
+                    }
+                },
+                {
+                    value: "Sun",
+                    label: {
+                        en: "Sunday",
+                        fr: "Dimanche",
+                        nl: "zondag"
+                    }
+                },
+                {
+                    value: "UNDEFINED",
+                    label: "UNDEFINED"
+                }
+            ]
+        },
+        {
+          name: "openFrom",
+            label: {},
+            type: "date",
+            admin: {
+              date: {
+                  pickerAppearance: "timeOnly"
+              }
             }
         },
         {
-            value: "Tu",
-            label: {
-                en: "Tuesday",
-                fr: "Mardi",
-                nl: "dinsdag"
+            name: "openTill",
+            label: {},
+            type: "date",
+            admin: {
+                date: {
+                    pickerAppearance: "timeOnly"
+                }
             }
-        },
-        {
-            value: "We",
-            label: {
-                en: "Wednesday",
-                fr: "Mercredi",
-                nl: "woensdag"
-            }
-        },
-        {
-            value: "Thu",
-            label: {
-                en: "Thursday",
-                fr: "Jeudi",
-                nl: "donderdag"
-            }
-        },
-        {
-            value: "Fr",
-            label: {
-                en: "Friday",
-                fr: "Vendredi",
-                nl: "vrijdag"
-            }
-        },
-        {
-            value: "Sat",
-            label: {
-                en: "Saturday",
-                fr: "Samedi",
-                nl: "zaterdag"
-            }
-        },
-        {
-            value: "Sun",
-            label: {
-                en: "Sunday",
-                fr: "Dimanche",
-                nl: "zondag"
-            }
-        },
-        {
-            value: "UNDEFINED",
-            label: "UNDEFINED"
         }
     ]
 }
