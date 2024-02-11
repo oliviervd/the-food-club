5ba98abbc360043dca4c04f93434ee154f9a6e01
oliviervd <oliviervandhuynslager@gmail.com>
2024-02-11 14:51:30 +0100

    remove bloated code after migration

--- src/fields/review.ts
+++ src/fields/review.ts
@@ -12,19 +12,7 @@ const review: Field = {
             name: "review",
             type: "richText",
             localized: true,
-            //defaultValue: "pending review",
             label: "review"
-        },
-        // leave fields below till migration strategy has been completed!
-        {
-            name: "reviewEN",
-            label: "review (EN)",
-            type: "richText"
-        },
-        {
-            name: "reviewNL",
-            label: "review (NL)",
-            type: "richText"
         },
         {
             name: "author",
